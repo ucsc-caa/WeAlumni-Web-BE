4184699f6438fe2eddb22ddecb06c89c745ef53e
Dun Liu <liudun199903@gmail.com>
2021-01-08 15:42:25 -0800

    CAA-123, Feature, Init Sprint Boot Project

---
+++ src/main/java/org/ucsccaa/homepagebe/HomepageBeApplication.java
@@ -0,0 +1,13 @@
+package org.ucsccaa.homepagebe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomepageBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomepageBeApplication.class, args);
+	}
+
+}
