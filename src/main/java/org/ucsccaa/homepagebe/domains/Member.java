52b745512d6fbe876c79bb21c71315d81d842d4a
Jiaying330 <jiaying.hou330@gmail.com>
2021-05-15 17:03:12 -0700

    CAA-243, Feature, Revise Member domain

---
+++ src/main/java/org/ucsccaa/homepagebe/domains/Member.java
@@ -0,0 +1,77 @@
+package org.ucsccaa.homepagebe.domains;
+
+import javax.persistence.*;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Member {
+    @Id
+    private Integer uid;
+    @Column(unique = true)
+    private Integer memberId;
+    @Enumerated(EnumType.STRING)
+    @Column(insertable = false, updatable = false)
+    private Status status;
+    private String name;
+    private Boolean gender;
+    private LocalDate birthday;
+    private String email;
+    private String phone;
+    private String wechat;
+    private Integer branch;
+    @Embedded
+    private Member.Address address;
+    @Embedded
+    private Member.Degree degree;
+    @Embedded
+    private Member.Career career;
+    private Boolean search;
+
+    public enum Status {
+        ACTIVE,
+        PENDING,
+        DEACTIVE
+    }
+
+    @Embeddable
+    @Data
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class Address {
+        private String street;
+        private String city;
+        private String country;
+        private String postal;
+    }
+
+    @Embeddable
+    @Data
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class Degree {
+        private String studentId;
+        private String program;
+        private Integer endYear;
+        private String major1;
+        private String major2;
+        private String minor;
+    }
+
+    @Embeddable
+    @Data
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class Career {
+        private Boolean status;
+        private String company;
+        private String position;
+    }
+}
