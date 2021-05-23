98a3cda50c5f670d231d447b797bed7023ac2c40
Jiaying330 <jiaying.hou330@gmail.com>
2021-05-23 15:12:18 -0700

    CAA-239, Feature, Member ServiceTest, ControllerTest, Article ServiceTest, ControllerTest

--- src/test/java/org/ucsccaa/homepagebe/controllers/ArticleControllerTest.java
+++ src/test/java/org/ucsccaa/homepagebe/controllers/ArticleControllerTest.java
@@ -1,190 +1,188 @@
 package org.ucsccaa.homepagebe.controllers;
-//
-//import static org.mockito.ArgumentMatchers.*;
-//import static org.mockito.Mockito.when;
-//
-//import com.fasterxml.jackson.databind.ObjectMapper;
-//
-//import org.assertj.core.util.Lists;
-//import org.junit.Before;
-//import org.junit.Test;
-//import org.junit.runner.RunWith;
-//import org.mockito.InjectMocks;
-//import org.mockito.Mock;
-//import org.mockito.Mockito;
-//import org.mockito.MockitoAnnotations;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.http.MediaType;
-//import org.springframework.test.context.junit4.SpringRunner;
-//import org.springframework.test.web.servlet.MockMvc;
-//import org.springframework.test.web.servlet.ResultMatcher;
-//import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-//import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
-//import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-//import org.ucsccaa.homepagebe.HomepageBeApplication;
-//import org.ucsccaa.homepagebe.domains.Article;
-//import org.ucsccaa.homepagebe.services.ArticleService;
-//
-//import javax.print.attribute.standard.Media;
-//import java.util.ArrayList;
-//import java.util.List;
-//
 
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.when;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import org.junit.Before;
 import org.junit.Test;
-import org.ucsccaa.homepagebe.utils.AppConstants;
-import org.ucsccaa.homepagebe.utils.CommonUtils;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.ucsccaa.homepagebe.domains.Article;
+import org.ucsccaa.homepagebe.exceptions.customizedExceptions.BadRequestException;
+import org.ucsccaa.homepagebe.services.ArticleService;
 
-import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
+@RunWith(SpringRunner.class)
 public class ArticleControllerTest {
+
+    protected MockMvc mockMvc;
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    @Mock
+    private ArticleService service;
+
+    @InjectMocks
+    private ArticleController controller;
+
+    private final List <String> resourceList = new ArrayList<>();
+    private final List<String> categories = new ArrayList<>();
+    private final List<Article> articleList = new ArrayList<>();
+    private final Article expectedArticle = new Article(1, "category", "title", "author", "brief","content", "cover", "timestamp", resourceList);
+
+    @Before
+    public void Before() {
+        MockitoAnnotations.initMocks(this);
+        mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
+    }
+
     @Test
-    public void test() {
-        System.out.println(CommonUtils.getCurrentDate());
-        System.out.println(CommonUtils.getCurrentDateTime());
+    public void getCategoriesTest() throws Exception{
+        when(service.getCategories()).thenReturn(categories);
+        MockHttpServletRequestBuilder builder = MockMvcRequestBuilders.get("/article/categories");
+
+        mockMvc.perform(builder)
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.payload").exists());
+    }
+
+    @Test
+    public void getArticleByCategoryTest() throws Exception{
+        when(service.getArticleByCategory(any(), any())).thenReturn(articleList);
+        MockHttpServletRequestBuilder builder = MockMvcRequestBuilders.get("/article/category/category/page/1");
+
+        mockMvc.perform(builder)
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.payload").exists());
+    }
+
+    @Test
+    public void getArticleTest() throws Exception {
+        when(service.getArticle(any())).thenReturn(expectedArticle);
+        MockHttpServletRequestBuilder builder = MockMvcRequestBuilders.get("/article/1");
+
+        mockMvc.perform(builder)
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.payload").exists());
+    }
+
+    @Test
+    public void getArticleTest_notfound() throws Exception {
+        when(service.getArticle(any())).thenReturn(null);
+        MockHttpServletRequestBuilder builder = MockMvcRequestBuilders.get("/article/1");
+
+        mockMvc.perform(builder)
+                .andExpect(MockMvcResultMatchers.status().isNotFound());
+    }
+
+    @Test
+    public void getArticleTest_exception() throws Exception {
+        when(service.getArticle(any())).thenThrow(new BadRequestException("Param id cannot be null"));
+        MockHttpServletRequestBuilder builder = MockMvcRequestBuilders.get("/article/1");
+
+        mockMvc.perform(builder)
+                .andExpect(MockMvcResultMatchers.status().is4xxClientError())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.message").value("Bad Request: reason - Param id cannot be null"));
+    }
+
+    @Test
+    public void getArticleInPagingTest() throws Exception {
+        when(service.getArticleByPaging(any())).thenReturn(articleList);
+        MockHttpServletRequestBuilder builder = MockMvcRequestBuilders.get("/article/page/1");
+
+        mockMvc.perform(builder)
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.payload").exists());
+    }
+
+    @Test
+    public void addArticleTest() throws Exception {
+        when(service.addArticle(any())).thenReturn(expectedArticle);
+        String json = objectMapper.writeValueAsString(expectedArticle);
+        MockHttpServletRequestBuilder builder = MockMvcRequestBuilders
+                .post("/article")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(json);
+
+        mockMvc.perform(builder)
+                .andExpect(MockMvcResultMatchers.status().isCreated())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.code").value(2102))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.message").value("Resource Created"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.payload.id").value(1));
+    }
+
+    @Test
+    public void addArticleTest_exception() throws Exception {
+        when(service.addArticle(any())).thenThrow(new BadRequestException("empty article"));
+        String json = objectMapper.writeValueAsString(expectedArticle);
+        MockHttpServletRequestBuilder builder = MockMvcRequestBuilders
+                .post("/article")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(json);
+
+        mockMvc.perform(builder)
+                .andExpect(MockMvcResultMatchers.status().is4xxClientError())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.message").value("Bad Request: reason - empty article"));
+    }
+
+    @Test
+    public void updateArticleTest() throws Exception {
+        when(service.updateArticle(any())).thenReturn(expectedArticle);
+        String json = objectMapper.writeValueAsString(expectedArticle);
+        MockHttpServletRequestBuilder builder = MockMvcRequestBuilders
+                .put("/article")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(json);
+
+        mockMvc.perform(builder)
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.payload.id").value(1));
+    }
+
+    @Test
+    public void updateArticleTest_exception() throws Exception {
+        when(service.updateArticle(any())).thenThrow(new BadRequestException("empty article"));
+        String json = objectMapper.writeValueAsString(expectedArticle);
+        MockHttpServletRequestBuilder builder = MockMvcRequestBuilders
+                .put("/article")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(json);
+
+        mockMvc.perform(builder)
+                .andExpect(MockMvcResultMatchers.status().is4xxClientError())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.message").value("Bad Request: reason - empty article"));
+    }
+
+    @Test
+    public void deleteArticleByIdTest() throws Exception {
+        MockHttpServletRequestBuilder builder = MockMvcRequestBuilders
+                .delete("/article/1");
+        mockMvc.perform(builder)
+                .andExpect(MockMvcResultMatchers.status().isOk());
+
+    }
+
+    @Test
+    public void deleteArticleByIdTest_exception() throws Exception {
+        doThrow(new BadRequestException("Param id cannot be null")).when(service).deleteArticleById(1);
+        MockHttpServletRequestBuilder builder = MockMvcRequestBuilders
+                .delete("/article/1");
+        mockMvc.perform(builder)
+                .andExpect(MockMvcResultMatchers.status().is4xxClientError())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.message").value("Bad Request: reason - Param id cannot be null"));
+
     }
 }
-//    protected MockMvc mockMvc;
-//    private final ObjectMapper objectMapper = new ObjectMapper();
-//    @Mock
-//    private ArticleService service;
-//    @InjectMocks
-//    private ArticleController controller;
-//    private final Article expectedArticle = new Article((long)1, "category", "title", "author", "content", null, "img", "html");
-//
-//    @Before
-//    public void Before() {
-//        MockitoAnnotations.initMocks(this);
-//        mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
-//    }
-//
-//    @Test
-//    public void addArticleTest() throws Exception {
-//        when(service.addArticle(any())).thenReturn(expectedArticle.getId());
-//        String json = objectMapper.writeValueAsString(expectedArticle);
-//        mockMvc.perform(MockMvcRequestBuilders.post("/articles")
-//                .contentType(MediaType.APPLICATION_JSON)
-//                .content(json))
-//                .andExpect(MockMvcResultMatchers.status().isOk())
-//                .andExpect(MockMvcResultMatchers.jsonPath("$.payload")
-//                        .value("http://localhost/articles/" + expectedArticle.getId().toString()));
-//    }
-//
-//    @Test
-//    public void addArticleNullTest() throws Exception {
-//        when(service.addArticle(null)).thenThrow(RuntimeException.class);
-//        String json = objectMapper.writeValueAsString(null);
-//        mockMvc.perform(MockMvcRequestBuilders.post("/articles")
-//                .contentType(MediaType.APPLICATION_JSON)
-//                .content(json))
-//                .andExpect(MockMvcResultMatchers.status().isBadRequest());
-//    }
-//
-//    @Test
-//    public void addArticleExceptionTest() throws Exception {
-//        when(service.addArticle(any())).thenThrow(RuntimeException.class);
-//        String json = objectMapper.writeValueAsString(expectedArticle);
-//        mockMvc.perform(MockMvcRequestBuilders.post("/articles")
-//                .contentType(MediaType.APPLICATION_JSON)
-//                .content(json))
-//                .andExpect(MockMvcResultMatchers.jsonPath("$.status").value("ERROR"));
-//    }
-//
-//    @Test
-//    public void updateArticleTest() throws Exception {
-//        String json = objectMapper.writeValueAsString(expectedArticle);
-//        when(service.updateArticle(any())).thenReturn(expectedArticle);
-//        mockMvc.perform(MockMvcRequestBuilders.put("/articles")
-//                .contentType(MediaType.APPLICATION_JSON)
-//                .content(json))
-//                .andExpect(MockMvcResultMatchers.status().isOk())
-//                .andExpect(MockMvcResultMatchers.jsonPath("$.payload.id").value(expectedArticle.getId()))
-//                .andExpect(MockMvcResultMatchers.jsonPath("$.payload.author").value(expectedArticle.getAuthor()));
-//    }
-//
-//    @Test
-//    public void updateArticleNotFoundTest() throws Exception {
-//        String json = objectMapper.writeValueAsString(expectedArticle);
-//        when(service.updateArticle(any())).thenReturn(null);
-//        mockMvc.perform(MockMvcRequestBuilders.put("/articles")
-//                .contentType(MediaType.APPLICATION_JSON)
-//                .content(json))
-//                .andExpect(MockMvcResultMatchers.status().isOk())
-//                .andExpect(MockMvcResultMatchers.jsonPath("$.status").value("NOT_FOUND"));
-//    }
-//
-//    @Test
-//    public void updateArticleExceptionTest() throws Exception {
-//        String json = objectMapper.writeValueAsString(expectedArticle);
-//        when(service.updateArticle(any())).thenThrow(RuntimeException.class);
-//        mockMvc.perform(MockMvcRequestBuilders.put("/articles")
-//                .contentType(MediaType.APPLICATION_JSON)
-//                .content(json))
-//                .andExpect(MockMvcResultMatchers.jsonPath("$.status").value("ERROR"));
-//    }
-//
-//    @Test
-//    public void getArticleByIdTest() throws Exception {
-//        when(service.getArticle(anyLong())).thenReturn(expectedArticle);
-//        mockMvc.perform(MockMvcRequestBuilders.get("/articles/" + expectedArticle.getId()))
-//                .andExpect(MockMvcResultMatchers.status().isOk())
-//                .andExpect(MockMvcResultMatchers.jsonPath("$.payload.id").value(expectedArticle.getId()))
-//                .andExpect(MockMvcResultMatchers.jsonPath("$.payload.author").value(expectedArticle.getAuthor()));
-//    }
-//
-//    @Test
-//    public void getArticleByIdNotFoundTest() throws Exception {
-//        when(service.getArticle(anyLong())).thenReturn(null);
-//        mockMvc.perform(MockMvcRequestBuilders.get("/articles/1"))
-//                .andExpect(MockMvcResultMatchers.status().isOk())
-//                .andExpect(MockMvcResultMatchers.jsonPath("$.status").value("NOT_FOUND"));
-//    }
-//
-//    @Test
-//    public void getArticleByIdExceptionTest() throws Exception {
-//        when(service.getArticle(anyLong())).thenThrow(RuntimeException.class);
-//        mockMvc.perform(MockMvcRequestBuilders.get("/articles/1"))
-//                .andExpect(MockMvcResultMatchers.jsonPath("$.status").value("ERROR"));
-//    }
-//
-//    @Test
-//    public void deleteArticleByIdTest() throws Exception {
-//        when(service.deleteArticleById(anyLong())).thenReturn(true);
-//        mockMvc.perform(MockMvcRequestBuilders.delete("/articles/1"))
-//                .andExpect(MockMvcResultMatchers.status().isOk())
-//                .andExpect(MockMvcResultMatchers.jsonPath("$.status").value("SUCCESS"));
-//    }
-//
-//    @Test
-//    public void deleteArticleByIdNotFoundTest() throws Exception {
-//        when(service.deleteArticleById(anyLong())).thenReturn(false);
-//        mockMvc.perform(MockMvcRequestBuilders.delete("/articles/1"))
-//                .andExpect(MockMvcResultMatchers.status().isOk())
-//                .andExpect(MockMvcResultMatchers.jsonPath("$.status").value("NOT_FOUND"));
-//    }
-//
-//    @Test
-//    public void deleteArticleByIdExceptionTest() throws Exception {
-//        when(service.deleteArticleById(anyLong())).thenThrow(RuntimeException.class);
-//        mockMvc.perform(MockMvcRequestBuilders.delete("/articles/1"))
-//                .andExpect(MockMvcResultMatchers.jsonPath("$.status").value("ERROR"));
-//    }
-//
-//    @Test
-//    public void getArticleByCategoryTest() throws Exception {
-//        List<Article> expected = Lists.list(expectedArticle, expectedArticle);
-//        when(service.getArticleByCategory(any(), any())).thenReturn(expected);
-//        mockMvc.perform(MockMvcRequestBuilders.get("/articles?category=fun&page=1")
-//                .contentType(MediaType.APPLICATION_JSON))
-//                .andExpect(MockMvcResultMatchers.status().isOk())
-//                .andExpect(MockMvcResultMatchers.jsonPath("$.status").value("SUCCESS"))
-//                .andExpect(MockMvcResultMatchers.jsonPath("$.payload").isArray());
-//    }
-//
-//    @Test
-//    public void getArticleByCategoryExceptionTest() throws Exception {
-//        when(service.getArticleByCategory(any(), any())).thenThrow(RuntimeException.class);
-//        mockMvc.perform(MockMvcRequestBuilders.get("/articles?category=fun&page=1")
-//                .contentType(MediaType.APPLICATION_JSON))
-//                .andExpect(MockMvcResultMatchers.jsonPath("$.status").value("ERROR"));
-//    }
-//}
