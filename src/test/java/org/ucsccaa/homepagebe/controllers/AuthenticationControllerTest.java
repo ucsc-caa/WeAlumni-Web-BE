432f30829c7e69c4acfa3dc31c435185dcba975f
Jiaying330 <jiaying.hou330@gmail.com>
2021-05-25 15:11:46 -0700

    CAA-252, feature, AuthenticationControllerTest (#31)

--- src/test/java/org/ucsccaa/homepagebe/controllers/AuthenticationControllerTest.java
+++ src/test/java/org/ucsccaa/homepagebe/controllers/AuthenticationControllerTest.java
@@ -1,60 +1,80 @@
-//package org.ucsccaa.homepagebe.controllers;
-//
-//import org.junit.Test;
-//import org.junit.runner.RunWith;
-//import org.mockito.InjectMocks;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-//import org.springframework.boot.test.mock.mockito.MockBean;
-//import org.springframework.http.MediaType;
-//import org.springframework.test.context.junit4.SpringRunner;
-//import org.springframework.test.web.servlet.MockMvc;
-//import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-//import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
-//import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
-//import org.ucsccaa.homepagebe.domains.User;
-//import org.ucsccaa.homepagebe.services.AuthenticationService;
-//
-//import static org.mockito.ArgumentMatchers.any;
-//import static org.mockito.Mockito.when;
-//
-//@RunWith(SpringRunner.class)
-//@WebMvcTest(AuthorizationController.class)
-//public class AuthorizationControllerTest {
-//    @Autowired
-//    protected MockMvc mockMvc;
-//    @MockBean
-//    private AuthenticationService authenticationService;
-//    @InjectMocks
-//    private UserController controller;
-//    private final User expectedUser = new User((long)1, "name", "password", "email", null);
-//
-//    @Test
-//    public void authenticateSuccessTest() throws Exception {
-//        when(authenticationService.authenticate(any(), any())).thenReturn(java.util.Optional.of(expectedUser));
-//        when(authenticationService.generateToken(any())).thenReturn("test_token");
-//        mockMvc.perform(MockMvcRequestBuilders.post("/authenticate")
-//                .param("username", "username")
-//                .param("password", "password")
-//                .contentType(MediaType.APPLICATION_JSON)
-//                .characterEncoding("UTF-8"))
-//                .andDo(MockMvcResultHandlers.print())
-//                .andExpect(MockMvcResultMatchers.status().isOk())
-//                .andExpect(MockMvcResultMatchers.jsonPath("$.status").value("SUCCESS"))
-//                .andExpect(MockMvcResultMatchers.jsonPath("$.payload").exists());
-//    }
-//
-//    @Test
-//    public void authenticateUserNotExistsTest() throws Exception {
-//        when(authenticationService.authenticate(any(), any())).thenReturn(java.util.Optional.ofNullable(null));
-//        when(authenticationService.generateToken(any())).thenReturn("test_token");
-//        mockMvc.perform(MockMvcRequestBuilders.post("/authenticate")
-//                .param("username", "username")
-//                .param("password", "password")
-//                .contentType(MediaType.APPLICATION_JSON)
-//                .characterEncoding("UTF-8"))
-//                .andDo(MockMvcResultHandlers.print())
-//                .andExpect(MockMvcResultMatchers.status().isOk())
-//                .andExpect(MockMvcResultMatchers.jsonPath("$.status").value("NOT_FOUND"));
-//    }
-//}
+package org.ucsccaa.homepagebe.controllers;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.ucsccaa.homepagebe.authentication.Authentication;
+import org.ucsccaa.homepagebe.domains.Member;
+import org.ucsccaa.homepagebe.domains.User;
+import org.ucsccaa.homepagebe.exceptions.customizedExceptions.UserNotFoundException;
+import org.ucsccaa.homepagebe.models.LoginResponse;
+import org.ucsccaa.homepagebe.services.UserService;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(AuthenticationController.class)
+public class AuthenticationControllerTest {
+
+    @Autowired
+    protected MockMvc mockMvc;
+
+    @MockBean
+    private Authentication authentication;
+
+    @MockBean
+    private UserService userService;
+
+    @InjectMocks
+    private AuthenticationController authenticationController;
+
+    private final User expectedUser = new User("email", "password", 1, true);
+    private final LoginResponse.BasicInfo basicInfo = new LoginResponse.BasicInfo(expectedUser.getUid(), 1, "name", expectedUser.getEmail(), Member.Status.PENDING, expectedUser.getEmailVerified());
+    private final String token = "eyJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE2MjE4OTQxOTgsImV4cCI6MTYyMTg5NTk5OCwidWlkIjoxLCJpc0FkbWluIjpmYWxzZX0.jkQJ8ft0_SNDwXK7lKltcBoeCdQx_dYejLkekXBwX80";
+
+    @Before
+    public void Before() {
+        MockitoAnnotations.initMocks(this);
+        mockMvc = MockMvcBuilders.standaloneSetup(authenticationController).build();
+    }
+
+    @Test
+    public void loginTest() throws Exception {
+        when(authentication.generateToken(any(), any())).thenReturn(token);
+        when(userService.getBasicInfoByEmail(any())).thenReturn(basicInfo);
+        MockHttpServletRequestBuilder builder = MockMvcRequestBuilders.post("/authenticate")
+                .param("email", "email")
+                .param("password", "password")
+                .contentType(MediaType.APPLICATION_JSON);
+
+        mockMvc.perform(builder)
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.payload").exists());
+    }
+
+    @Test
+    public void loginTest_exception() throws Exception {
+        when(authentication.generateToken(any(), any())).thenThrow(new UserNotFoundException("email"));
+        MockHttpServletRequestBuilder builder = MockMvcRequestBuilders.post("/authenticate")
+                .param("email", "email")
+                .param("password", "password")
+                .contentType(MediaType.APPLICATION_JSON);
+
+        mockMvc.perform(builder)
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.message").value("User Not Found: user - email"));
+    }
+}
