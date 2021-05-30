ec0afb636173071de13ac7db5f7e7d8804dc3b95
liehuozhizun <hyuan3@ucsc.edu>
2021-05-30 08:48:42 -0700

    Fix CORS issue

--- src/main/java/org/ucsccaa/homepagebe/configurations/GlobalCorsConfiguration.java
+++ src/main/java/org/ucsccaa/homepagebe/configurations/GlobalCorsConfiguration.java
@@ -3,12 +3,17 @@ package org.ucsccaa.homepagebe.configurations;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @Configuration
-public class GlobalCorsConfiguration {
+public class GlobalCorsConfiguration  implements HandlerInterceptor {
     
     @Bean
     public FilterRegistrationBean corsFilter() {
@@ -18,9 +23,22 @@ public class GlobalCorsConfiguration {
 		config.addAllowedOrigin("*");
 		config.addAllowedHeader("*");
 		config.addAllowedMethod("*");
-		source.registerCorsConfiguration("/**", config);
+		source.registerCorsConfiguration("/**/*", config);
 		FilterRegistrationBean bean = new FilterRegistrationBean(new CorsFilter(source));
 		bean.setOrder(0);
 		return bean;
+	}
+
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object o) {
+		if (request.getHeader(HttpHeaders.ORIGIN) != null) {
+			String origin = request.getHeader("Origin");
+			response.addHeader("Access-Control-Allow-Origin", origin);
+			response.addHeader("Access-Control-Allow-Credentials", "true");
+			response.addHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, PUT,PATCH, HEAD");
+			response.addHeader("Access-Control-Allow-Headers", "Content-Type");
+			response.addHeader("Access-Control-Max-Age", "3600");
+		}
+		return true;
 	}
 }
